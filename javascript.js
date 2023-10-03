abd48c24d4cdbf97e32c92ef2fb30a2b93b165d2
Gabriel McAlister <gmcalister88@gmail.com>
2023-10-03 19:05:40 -0400

    Fixed functions, added html

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 const dialog = document.querySelector('dialog');
 const showButton = document.querySelector('dialog + button');
 const closeButton = document.querySelector('dialog button');
+const submitButton = document.querySelector('.submitButton')
+
+const titleInput = document.querySelector('#titleInput');
+const authorInput = document.querySelector('#authorInput');
+const pageInput = document.querySelector('#pageInput');
+const readInput = document.querySelector('#readInput');
+
+const myLibrary = [];
 
 showButton.addEventListener("click", () => {
     dialog.showModal();
@@ -10,7 +18,36 @@ closeButton.addEventListener("click", () =>{
     dialog.close();
 });
 
-const myLibrary = [];
+submitButton.addEventListener('click', () =>{
+    const book = new Book(titleInput.value, authorInput.value, pageInput.value, readInput.value)
+    addBookToLibrary(book);
+    dialog.close();
+});
+
+
+
+function addRow(title, author, pages, read){
+    let table = document.querySelector("table")
+
+    let row = document.createElement("tr");
+    let c1 = document.createElement("td");
+    let c2 = document.createElement('td');
+    let c3 = document.createElement("td");
+    let c4 = document.createElement('td');
+
+    c1.innerHTML = title;
+    c2.innerHTML = author;
+    c3.innerHTML = pages;
+    c4.innerHTML = read;
+
+    row.appendChild(c1);
+    row.appendChild(c2);
+    row.appendChild(c3);
+    row.appendChild(c4);
+
+    table.appendChild(row);
+}
+
 
 function Book(title, author, pages, read) {
     this.title = title,
@@ -19,32 +56,25 @@ function Book(title, author, pages, read) {
     this.read = read
     
     function info(){
-        if(read == true){
-            return title + ' by ' + author + ', ' + pages + ' pages, read'
-        }
-        else{
-            return title + ' by ' + author + ', ' + pages + ' pages, not read'
-        }
+        return title + ' by ' + author + ', ' + pages + ' pages, ' + read
     }
-}
-
-function addBookToLibrary(Book) {
-  myLibrary.push(Book);
 }
 
 
 function displayLibrary(){
     console.table(myLibrary);
+    for(i = 0; i < myLibrary.length; i++){
+        addRow[myLibrary[i].title, myLibrary[i].author, myLibrary[i].pages, myLibrary[i].read];
+    }
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  displayLibrary();
 }
 
 
-const book1 = new Book("A Book", "Me", "456", true)
-const book2 = new Book("Another Book", "Other Person", '2', false)
-const book3 = new Book('Yet another Book', 'Hamswith', '521', false);
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
 
-displayLibrary();
+
 
